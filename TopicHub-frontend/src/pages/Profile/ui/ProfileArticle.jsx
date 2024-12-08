f2c5cbcede971d319047fe3cbf52e77f756ac518
evgluzedev <evgeniy.shostak.04@mail.ru>
2024-12-08 16:34:55 +0300

    refactoring

---
+++ TopicHub-frontend/src/pages/Profile/ui/ProfileArticle.jsx
@@ -0,0 +1,141 @@
+import { Box, Button, Typography } from "@mui/material";
+import MenuWrapper from "../../../widgets/menu/ui/MenuWrapper";
+import { Link, useNavigate } from "react-router";
+import { PathConstants } from "../../../app/pathConstants";
+
+import { getArticle } from "../../../features/Article/model/articleSlice";
+import { useDispatch, useSelector } from "react-redux";
+import CommentsList from "../../../widgets/comments/ui/CommentsList";
+import Article from './../../../features/Article/ui/Article';
+import ConfirmModal from "../../../shared/ConfirmModal/ui/ConfirmModal";
+import { useEffect, useState } from "react";
+import { delUserArticle } from "../api/requests";
+import { controlUserStatus, getUserStatus } from "../model/userSlice";
+import statusTypes from "../../../app/util/statusTypes";
+
+function ProfileArticle() {
+    const navigation = useNavigate()
+    const article = useSelector(getArticle)
+    const [open,setOpen] = useState(false)
+    const dispatch = useDispatch()
+    const status = useSelector(getUserStatus)
+
+    const handleDelete=()=>{
+        setOpen(true)
+    }
+    const handleEdit=()=>{
+       
+       
+    }
+
+    const handlerAgreehandle=(id)=>{
+        dispatch(delUserArticle(
+
+            {
+                id:article.id,
+                type:"article"
+            }
+
+        ))
+        setOpen(false)
+    }
+
+    const handlerDisagree=()=>{
+        setOpen(false)
+    }
+
+    // useEffect(()=>{
+
+    //     if(status==statusTypes.succeeded){
+    //         navigation(PathConstants.PROFILE)
+    //         dispatch(controlUserStatus(statusTypes.idle))
+    //     }
+
+    // },[status])
+
+    return (
+
+        <>
+        
+        
+        
+      <Box 
+      
+      sx={{
+          display: "flex",
+          flexDirection: "column",
+          maxWidth: "1000px",
+          margin: "0 auto",
+        }}
+      
+      >
+        <MenuWrapper>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "flex-start",
+              alignItems: "center",
+              width: "100%",
+            }}
+          >
+            <Link to={PathConstants.PROFILE}>
+            
+                <Typography
+                  variant="body1"
+                  style={{ textDecoration:"underline" }}
+                >
+                  Назад
+                </Typography>
+              
+            </Link>
+          </Box>
+        </MenuWrapper>
+        
+  
+        <Box sx={{margin:"20px 0 20px 0"}} >
+              <Box sx={{maxWidth:"820px", display:"flex", flexDirection:"column", margin:"0 auto",gap:"10px"}} >
+                  <Article item={article} mode={"long"} edit={true} handleEdit={handleEdit} handleDelete={handleDelete}/>
+  
+                  <MenuWrapper>
+                      <Box sx={{display:"flex", justifyContent:"space-between",alignItems:"center",width:"100%"}}>
+                          <Box   sx={{display:"flex", flexDirection:"row", gap:"10px"}}>
+  
+                          <img alt="лого" />
+                          <Typography>Имя автора</Typography>
+  
+                          </Box>
+                      <Button variant="outlined" color="success">Подписаться</Button>
+                      </Box>
+                  </MenuWrapper>
+                  
+                  <MenuWrapper>
+                     <CommentsList
+                     
+                     article={{}}
+                     
+                     />
+                  </MenuWrapper>
+  
+              </Box>
+  
+        </Box>
+  
+       
+       
+      </Box>
+
+      <ConfirmModal
+      
+      show={open}
+      title={"Удаление статьи"}
+      body={"Вы действительно хотите удалить статью?"}
+      handlerAgree={handlerAgreehandle}
+      handlerDisagree={handlerDisagree}
+      data={article}
+      />
+
+      </>
+    );
+}
+
+export default ProfileArticle;
